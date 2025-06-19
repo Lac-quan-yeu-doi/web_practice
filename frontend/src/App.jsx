1f50b7b7be33b03ecebfcf61f6bf9a590ff5abc4
Lac-quan-yeu-doi <nguyenphat4002@gmail.com>
2025-06-20 02:46:56 +0700

    COMPLETED

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import { Box, Button, useColorModeValue } from '@chakra-ui/react';
 import {Route, Routes} from 'react-router-dom';
-import NavBar from 'components/Navbar.jsx';
-import HomePage from 'pages/HomePage.jsx';
-import CreatePage from 'pages/CreatePage.jsx';
+import NavBar from './components/NavBar.jsx';
+import HomePage from './pages/HomePage.jsx';
+import CreatePage from './pages/CreatePage.jsx';
 
 function App() {
 
